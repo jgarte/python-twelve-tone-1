3eac7c69bd4e2bb7d16daaa4f260a3dcc14a4c24
jgart <jgart@dismail.de>
2023-06-15 10:10:50 -0500

    Simplify CLI.

--- src/twelve_tone/cli.py
+++ src/twelve_tone/cli.py
@@ -14,27 +14,13 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+# TODO: Use argparse instead.
 import click
 
 from twelve_tone.composer import Composer
 
 
 @click.command()
-@click.option('--row', '-r', default=0, help='Row to use as row tone')
-@click.option('--column', '-c', default=0, help='Column to use as column tone')
-@click.option('--midi', '-m', help='MIDI output file')
-def main(row, column, midi):
+def main():
     c = Composer()
-    c.compose()
-    if row < 0 or column < 0:
-        click.echo("Invalid row or column arguments.")
-        exit(1)
-    elif row >= c.matrix.shape[0]:
-        click.echo("Row number exceeds melody row count.")
-        exit(1)
-    elif column >= c.matrix.shape[1]:
-        click.echo("Column number exceeds melody column count.")
-        exit(1)
-    click.echo(c.get_melody(row=row, column=column))
-    if midi is not None:
-        c.save_to_midi(filename=midi)
+    click.echo(c.play_tone_row())
