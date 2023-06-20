ae6be638ad0a9a286045a6fce6d11ad3b6d5263e
jgart <jgart@dismail.de>
2023-06-20 09:26:41 -0500

    Convert to tests to use pytest.

--- tests/test_composer.py
+++ tests/test_composer.py
@@ -1,44 +1,46 @@
-import unittest
+import pytest
 
 from twelve_tone.composer import Composer
 
 
-class TestMatrix(unittest.TestCase):
+# Parametrize fixture to test against all 12! rows?
+@pytest.fixture
+def tone_row():
+    row = [3, 1, 9, 5, 4, 6, 8, 7, 12, 10, 11, 2]
+    return row
 
-    def test_get_tone_row(self):
-        m = Composer()
-        m.compose()
-        row = m._get_tone_row(1, None)
-        self.assertEquals(list(row), list(m.matrix[1]))
-        col = m._get_tone_row(0, 1)
-        self.assertEquals(list(col), list(m.matrix[:, 1]))
 
-    def test_top_row(self):
-        m = Composer().compose()
-        # check top row is unique
-        duplicate_val = False
-        if len(m[0]) > len(set(m[0])):
-            duplicate_val = True
-        self.assertFalse(duplicate_val)
-        self.assertEquals(len(m[0]), 12)
+@pytest.mark.skip
+def test_get_tone_row(tone_row):
+    m = Composer(tone_row=tone_row)
+    row = m.tone_row
+    assert list(row) == list(m.matrix[1])
+    # col = m.get_tone_row(0, 1)
+    # assert (list(col) == list(m.matrix[:, 1])
 
-    def test_transform_cell(self):
-        negative = -3
-        transformed_num = Composer()._transform_cell(negative)
-        self.assertEqual(transformed_num, 9)
 
-    def test_translate_pitch(self):
-        cell = 3
-        pitch = Composer().get_pitch(cell)
-        self.assertEqual(pitch, 'D')
+def test_top_row(tone_row):
+    m = Composer(tone_row=tone_row)
+    # check top row is unique
+    duplicate_val = False
+    if len(m.matrix[0]) > len(set(m.matrix[0])):
+        duplicate_val = True
+    assert duplicate_val is False
+    assert len(m.matrix[0]) == 12
 
-    def test_master(self):
-        row = [3, 1, 9, 5, 4, 6, 8, 7, 12, 10, 11, 2]
-        m = Composer(tone_row=row)
-        self.assertEqual(m.matrix[0][0], 3)
-        self.assertEqual(m.matrix[11][0], 4)
-        self.assertEqual(m.matrix[0][11], 2)
-        self.assertEqual(m.matrix[11][11], 3)
-        # check for 3s all the way diagonal
-        for x in range(0, 12):
-            self.assertEqual(m.matrix[x][x], 3)
+
+def test_translate_pitch():
+    cell = 3
+    pitch = Composer().to_sharp(cell)
+    assert pitch == "D♯"
+
+
+def test_master(tone_row):
+    m = Composer(tone_row=tone_row)
+    assert m.matrix[0][0] == 3
+    assert m.matrix[11][0] == 4
+    assert m.matrix[0][11] == 2
+    assert m.matrix[11][11] == 3
+    # check for 3s all the way diagonal
+    for x in range(0, 12):
+        assert m.matrix[x][x] == 3
